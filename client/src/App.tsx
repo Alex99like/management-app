31bec072e64e2274185791a69f35154adda18dfc
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-11-12 16:19:12 +0300

    fix: fix routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 import { publiRouter } from './config/routes';
 import { RootProvider } from './providers/RootProvider';
 
 function App() {
-  return (
-    <RootProvider>
-      <RouterProvider router={publiRouter} />;
-    </RootProvider>
-  );
+  return <RouterProvider router={publiRouter} />;
 }
 
 export default App;
