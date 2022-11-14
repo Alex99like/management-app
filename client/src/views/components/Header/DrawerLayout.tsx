796705e52d38096d1517040fe6f74f3465c76844
Maria Guchkova <mariaguchkova1@gmail.com>
2022-11-14 17:35:08 +0300

    refactor: create separate menu item component

--- client/src/views/components/Header/DrawerLayout.tsx
+++ client/src/views/components/Header/DrawerLayout.tsx
@@ -1,20 +1,11 @@
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Divider,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-} from '@mui/material';
+import { Box, Button, ButtonGroup, Divider, Drawer, List } from '@mui/material';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
-import Buttons from './Buttons';
+import AuthButton from '../Button/Button';
+import MenuItem from './MenuItem';
 
 type DrawerLayoutPropsType = {
   menuOpen: boolean;
@@ -32,40 +23,17 @@ const DrawerLayout: React.FC<DrawerLayoutPropsType> = ({ menuOpen, closeMenu }) 
       <Box sx={{ width: '100%', marginTop: 9.5 }} onClick={closeMenu}>
         <List>
           <Link style={linkStyle} to="/">
-            <ListItem key={'Welcome'}>
-              <ListItemButton>
-                <ListItemIcon>
-                  <HomeOutlinedIcon color="primary" />
-                </ListItemIcon>
-                <p>Welcome</p>
-              </ListItemButton>
-            </ListItem>
+            <MenuItem icon={<HomeOutlinedIcon color="primary" />} caption="Welcome" />
           </Link>
         </List>
         <List>
           <Link style={linkStyle} to="/main">
-            <ListItem key={'Boards'}>
-              <ListItemButton>
-                <ListItemIcon>
-                  <DashboardIcon color="primary" />
-                </ListItemIcon>
-                <p>Boards</p>
-              </ListItemButton>
-            </ListItem>
+            <MenuItem icon={<DashboardIcon color="primary" />} caption="Boards" />
           </Link>
         </List>
-        <List>
-          <ListItem key={'Edit Profile'}>
-            <ListItemButton>
-              <ListItemIcon>
-                <AccountCircleIcon color="primary" />
-              </ListItemIcon>
-              <p>Edit Profile</p>
-            </ListItemButton>
-          </ListItem>
-        </List>
+        <MenuItem icon={<AccountCircleIcon color="primary" />} caption="Edit Profile" />
         <Divider />
-        <Buttons />
+        <AuthButton title="Authorize" />
         <Divider />
         <ButtonGroup variant="text" className={styles.buttonGroup}>
           <Button>English</Button>
