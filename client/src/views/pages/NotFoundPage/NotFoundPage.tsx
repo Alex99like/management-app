31624ff9382284e2ec1c8106fe9c8a1d290abfb5
Maria Guchkova <mariaguchkova1@gmail.com>
2022-11-16 15:42:55 +0300

    feat: add not found page

--- client/src/views/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/views/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,28 @@
-function NotFound() {
-  return <div>Not found</div>;
+import { useAuth } from '../../../hooks/useAuth';
+import Button from '../../components/Button/Button';
+import styles from './NotFoundPage.module.scss';
+
+function NotFoundPage() {
+  const classNames: string[] = ['last', 'second', 'first', 'basic', 'first', 'second', 'last'];
+  const error = 401;
+  const { routes } = useAuth();
+
+  return (
+    <div className={styles.notFound}>
+      <div className={styles.error}>
+        {classNames.map((className, index) => (
+          <p className={styles[className]} key={error + index}>
+            {error + index}
+          </p>
+        ))}
+      </div>
+      <p className={styles.title}>The page you were looking for could not be found</p>
+      <Button
+        title={routes === 'private' ? 'Go To Welcome Page' : 'Go To Main Page'}
+        link={routes === 'private' ? '/' : '/main'}
+      />
+    </div>
+  );
 }
 
-export default NotFound;
+export default NotFoundPage;
