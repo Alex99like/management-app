7c1a5873643f370aaf3a6d214a954300c2b0e7fe
Maria Guchkova <mariaguchkova1@gmail.com>
2022-11-17 14:52:21 +0300

    feat: add confirmation modal

--- client/src/views/components/Board/Board.tsx
+++ client/src/views/components/Board/Board.tsx
@@ -1,24 +1,34 @@
 import styles from './Board.module.scss';
 import deleteImg from '../../../assets/icons/delete.svg';
 import { useNavigate } from 'react-router-dom';
+import ConfirmationModal from '../ConfirmationModal/ConfirmationModal';
+import { useState } from 'react';
 
 function Board(props: { title: string; description?: string }) {
   const { title, description } = props;
+  const [openModal, setOpenModal] = useState(false);
 
   const navigate = useNavigate();
 
-  function handleClick() {
-    navigate('/board');
+  function handleClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    if ((e.target as HTMLDivElement).tagName !== 'IMG') {
+      navigate('/board');
+    } else {
+      setOpenModal(true);
+    }
   }
 
   return (
-    <div className={styles.board} onClick={handleClick}>
-      <div className={styles.container}>
-        <h4 className={styles.title}>{title}</h4>
-        <img className={styles.image} src={deleteImg} alt="delete" />
+    <>
+      <div className={styles.board} onClick={(e) => handleClick(e)}>
+        <div className={styles.container}>
+          <h4 className={styles.title}>{title}</h4>
+          <img className={styles.image} src={deleteImg} alt="delete" />
+        </div>
+        <p className={styles.description}>{description ? description : 'No description'}</p>
       </div>
-      <p className={styles.description}>{description ? description : 'No description'}</p>
-    </div>
+      <ConfirmationModal open={openModal} setOpen={setOpenModal} />
+    </>
   );
 }
 
---
+++ client/src/views/components/ConfirmationModal/ConfirmationModal.tsx
@@ -0,0 +1,60 @@
+import { Box, Divider, Modal } from '@mui/material';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import styles from './ConfirmationModal.module.scss';
+import cn from 'classnames';
+
+function ConfirmationModal(props: { open: boolean; setOpen: Dispatch<SetStateAction<boolean>> }) {
+  const { open, setOpen } = props;
+  const [active, setActive] = useState(false);
+  const [switcherUp, setSwitcherUp] = useState(false);
+  const [switcherDown, setSwitcherDown] = useState(false);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    setSwitcherUp(true);
+    setTimeout(() => {
+      setSwitcherUp(false);
+      setSwitcherDown(true);
+      setTimeout(() => {
+        setSwitcherDown(false);
+      }, 150);
+    }, 150);
+  }, [open]);
+
+  useEffect(() => {
+    setActive(true);
+  }, []);
+
+  return (
+    <Modal
+      className={styles.confirmationModal}
+      open={open}
+      onClose={handleClose}
+      disableAutoFocus={true}
+    >
+      <Box
+        className={cn(styles.box, {
+          [styles.switcherUp]: switcherUp,
+          [styles.switcherDown]: switcherDown,
+          [styles.active]: active,
+        })}
+      >
+        <div className={styles.message}>
+          <h4>Are you sure?</h4>
+          <p>All board data will be deleted.</p>
+        </div>
+        <Divider />
+        <div className={styles.buttons}>
+          <button className={styles.button} onClick={handleClose}>
+            OK
+          </button>
+          <button className={styles.button} onClick={handleClose}>
+            Cancel
+          </button>
+        </div>
+      </Box>
+    </Modal>
+  );
+}
+
+export default ConfirmationModal;
