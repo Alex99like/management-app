657807b3bf0c8a5611f479eae002816bc36fea38
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-11-18 18:37:02 +0300

    feat: add task query

--- client/src/services/Task.service.ts
+++ client/src/services/Task.service.ts
@@ -9,13 +9,33 @@ export const tasksApi = api.injectEndpoints({
     }),
     createTask: builder.mutation<ITask, { task: ITaskReq; boardId: string; columnsId: string }>({
       query: ({ boardId, columnsId, task }) => ({
-        url: `boards/${boardId}/columns/${columnsId}`,
+        url: `boards/${boardId}/columns/${columnsId}/tasks`,
         method: 'POST',
         body: task,
       }),
-      invalidatesTags: () => [{ type: 'Columns' }],
+      invalidatesTags: () => [{ type: 'Tasks' }],
+    }),
+    updateTask: builder.mutation<ITask, { task: Omit<ITask, 'id'>; taskId: string }>({
+      query: ({ taskId, task }) => ({
+        url: `boards/${task.boardId}/columns/${task.columnId}/tasks/${taskId}`,
+        method: 'PUT',
+        body: task,
+      }),
+      invalidatesTags: () => [{ type: 'Tasks' }],
+    }),
+    deleteTask: builder.mutation<void, { boardId: string; columnsId: string; taskId: string }>({
+      query: ({ taskId, boardId, columnsId }) => ({
+        url: `boards/${boardId}/columns/${columnsId}/tasks/${taskId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: () => [{ type: 'Tasks' }],
     }),
   }),
 });
 
-export const { useGetTasksQuery } = tasksApi;
+export const {
+  useGetTasksQuery,
+  useCreateTaskMutation,
+  useUpdateTaskMutation,
+  useDeleteTaskMutation,
+} = tasksApi;
--- client/src/views/pages/MainPage/MainPage.tsx
+++ client/src/views/pages/MainPage/MainPage.tsx
@@ -1,24 +1,13 @@
-import { useDeleteBoardMutation, useUpdateBoardMutation } from '../../../services/Board.service';
-import { useDeleteColumnMutation, useUpdateColumnMutation } from '../../../services/Column.service';
+import { useGetUsersQuery } from '../../../services/Api.service';
+import { useGetBoardsQuery } from '../../../services/Board.service';
 import Board from '../../components/Board/Board';
 import styles from './MainPage.module.scss';
 
 function MainPage() {
-  const [create, { data }] = useDeleteColumnMutation();
-
-  const handle = () => {
-    create({
-      boardId: '57d9f3af-216b-43f7-bd41-14fc32b63f28',
-      columnsId: '1892f575-dddb-440e-b409-0d538861658d',
-    });
-  };
-  // console.log(data);
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
         <h3>Your Boards</h3>
-        <button onClick={handle}>Create</button>
         <div className={styles.boards}>
           <Board title="task" />
           <Board title="task" />
