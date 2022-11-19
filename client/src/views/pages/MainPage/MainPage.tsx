cee718335092773faf15ce194c65f27b0c78d85c
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-11-19 05:50:09 +0300

    feat: add loader

--- client/src/views/pages/MainPage/MainPage.tsx
+++ client/src/views/pages/MainPage/MainPage.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 import { toastr } from 'react-redux-toastr';
-import { useGetUsersQuery } from '../../../services/Api.service';
 import { useCreateBoardMutation, useGetBoardsQuery } from '../../../services/Board.service';
 import { IBoardReq } from '../../../types/board.type';
 import { MaterialIconBS } from '../../../utils/MaterialIcon';
 import Board from '../../components/Board/Board';
 import { FormBoard } from '../../components/FormBoard/FormBoard';
 import styles from './MainPage.module.scss';
+import Lottie from 'lottie-react';
+import Loader from '../../../assets/animation/loder-border.json';
+import cn from 'classnames';
 
 function MainPage() {
-  const { data } = useGetBoardsQuery();
+  const { data, isLoading } = useGetBoardsQuery();
   const [create, { isSuccess, data: dataItem }] = useCreateBoardMutation();
   const [activeForm, setActiveForm] = useState(false);
 
@@ -18,7 +20,8 @@ function MainPage() {
       toastr.success('Success!', `Board created ${dataItem ? dataItem.title : ''} !`);
       setActiveForm(false);
     }
-  }, [isSuccess]);
+  }, [dataItem, isSuccess]);
+  console.log(isLoading);
 
   const handleCreateBoard = (data: IBoardReq) => {
     create(data);
@@ -26,6 +29,12 @@ function MainPage() {
 
   return (
     <>
+      {isLoading && (
+        <Lottie
+          className={cn(styles.loader, { [styles.active]: isLoading })}
+          animationData={Loader}
+        />
+      )}
       {activeForm && (
         <FormBoard
           createBoard={handleCreateBoard}
@@ -37,14 +46,16 @@ function MainPage() {
         <div className={styles.main}>
           <h3>Your Boards</h3>
           <div className={styles.boards}>
-            <button className={styles.newBoard} onClick={() => setActiveForm(true)}>
-              <div className={styles.dashed}>
-                <span className={styles.add}>
-                  <MaterialIconBS name={'BsPlusLg'} />
-                </span>
-                <h3 className={styles.title}>Add New Board</h3>
-              </div>
-            </button>
+            {!isLoading && (
+              <button className={styles.newBoard} onClick={() => setActiveForm(true)}>
+                <div className={styles.dashed}>
+                  <span className={styles.add}>
+                    <MaterialIconBS name={'BsPlusLg'} />
+                  </span>
+                  <h3 className={styles.title}>Add New Board</h3>
+                </div>
+              </button>
+            )}
             {data && data.map((board) => <Board key={board.id} board={board} />)}
           </div>
         </div>
