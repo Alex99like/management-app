3b697f237c489c2925e40c23d73ece793baa9fee
muannna <evagoeshard@gmail.com>
2022-11-24 06:12:30 +0500

    feat: add task draggable

--- client/src/views/components/Task/Task.tsx
+++ client/src/views/components/Task/Task.tsx
@@ -1,43 +1,22 @@
 import styles from './Task.module.scss';
 import dotsImg from '../../../assets/icons/dots.svg';
-import { DragEvent, useState } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
-function Task(props: { task: string }) {
-  const [currentTask, setCurrentTask] = useState<null | { task: string }>(null);
-
-  function dragStartHandler(e: DragEvent<HTMLDivElement>, task: { task: string }) {
-    console.log('drag', task);
-    setCurrentTask(task);
-  }
-
-  function dragEndHandler(e: DragEvent<HTMLDivElement>): void {
-    e.currentTarget.style.background = 'white';
-  }
-
-  function dragOverHandler(e: DragEvent<HTMLDivElement>): void {
-    e.preventDefault();
-    e.currentTarget.style.background = 'grey'; //стили надо будет поменять
-  }
-
-  function dropHandler(e: DragEvent<HTMLDivElement>, task: { task: string }) {
-    e.preventDefault();
-    console.log('drop', task);
-    e.currentTarget.style.background = 'white';
-  }
-
+function Task(props: { task: string; id: number; index: number }) {
   return (
-    <div
-      className={styles.task}
-      draggable={true}
-      onDragStart={(e) => dragStartHandler(e, props)}
-      onDragEnd={(e) => dragEndHandler(e)}
-      onDragOver={(e) => dragOverHandler(e)}
-      onDragLeave={(e) => dragEndHandler(e)}
-      onDrop={(e) => dropHandler(e, props)}
-    >
-      <p>{props.task}</p>
-      <img className={styles.image} src={dotsImg} alt="edit" />
-    </div>
+    <Draggable draggableId={`${props.id}`} index={props.index}>
+      {(provided) => (
+        <div
+          className={styles.task}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <p>{props.task}</p>
+          <img className={styles.image} src={dotsImg} alt="edit" />
+        </div>
+      )}
+    </Draggable>
   );
 }
 
