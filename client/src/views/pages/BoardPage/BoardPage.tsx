67c8941548294a8b395b4f1314cdeab99eb2f5a9
muannna <evagoeshard@gmail.com>
2022-11-24 06:11:30 +0500

    feat: add dragdropcontext

--- client/src/views/pages/BoardPage/BoardPage.tsx
+++ client/src/views/pages/BoardPage/BoardPage.tsx
@@ -3,12 +3,31 @@ import AddButton from '../../components/AddButton/AddButton';
 import arrow from '../../../assets/icons/icon-arrow-right.svg';
 import Column from '../../components/Column/Column';
 import styles from './BoardPage.module.scss';
+import { useState } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 function BoardPage() {
-  const columns = [];
-  for (let i = 0; i < 6; i++) {
-    columns.push(<Column key={i + 1} id={`${i + 1}`} title={`column${i + 1}`} />);
-  }
+  const initialData = {
+    tasks: [
+      { id: 1, task: 'task1' },
+      { id: 2, task: 'task2' },
+      { id: 3, task: 'task3' },
+      { id: 4, task: 'task4' },
+    ],
+    columns: [
+      { id: 1, title: 'column1', order: [0, 1, 2, 3] },
+      { id: 2, title: 'column2', order: [0, 1, 2, 3] },
+      { id: 3, title: 'column3', order: [0, 1, 2, 3] },
+      { id: 4, title: 'column4', order: [0, 1, 2, 3] },
+      { id: 5, title: 'column5', order: [0, 1, 2, 3] },
+      { id: 6, title: 'column6', order: [0, 1, 2, 3] },
+    ],
+    columnOrder: [0, 1, 2, 3, 4, 5],
+  };
+
+  const [data, setData] = useState(initialData);
+
+  function dragEndHandler() {}
 
   return (
     <div className={styles.wrapper}>
@@ -20,7 +39,15 @@ function BoardPage() {
         <h3>Task title</h3>
       </div>
       <div className={styles.board}>
-        {columns}
+        <DragDropContext onDragEnd={() => dragEndHandler()}>
+          {data &&
+            data.columnOrder.map((columnId) => {
+              const column = data.columns[columnId];
+              const tasks = column.order.map((taskId) => data.tasks[taskId]);
+
+              return <Column key={column.id} id={column.id} title={column.title} tasks={tasks} />;
+            })}
+        </DragDropContext>
         <div>
           <AddButton title="column" />
         </div>
