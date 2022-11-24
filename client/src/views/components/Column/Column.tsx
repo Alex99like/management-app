928fdf8e34f37257080d8a6c7ad793429dad4136
muannna <evagoeshard@gmail.com>
2022-11-24 06:12:05 +0500

    feat: add column droppable

--- client/src/views/components/Column/Column.tsx
+++ client/src/views/components/Column/Column.tsx
@@ -4,29 +4,35 @@ import Task from '../Task/Task';
 import AddButton from '../AddButton/AddButton';
 import { useState } from 'react';
 import ConfirmationModal from '../ConfirmationModal/ConfirmationModal';
+import { Droppable } from 'react-beautiful-dnd';
 
-function Column(props: { title: string; id: string }) {
+function Column(props: { title: string; id: number; tasks: { id: number; task: string }[] }) {
   const [openModal, setOpenModal] = useState(false);
-  const tasks = [];
-  for (let i = 0; i < 4; i++) {
-    tasks.push(<Task task={`task${i + 1}`} key={i + 1} />);
-  }
 
   return (
-    <div className={styles.column}>
-      <div className={styles.container}>
-        <h4 className={styles.title}>{props.title}</h4>
-        <img
-          className={styles.image}
-          src={deleteImg}
-          alt="delete"
-          onClick={() => setOpenModal(true)}
-        />
-      </div>
-      <div className={styles.tasks}>{tasks}</div>
-      <AddButton title="task" />
-      <ConfirmationModal id={'1'} open={openModal} setOpen={setOpenModal} title="Column" />
-    </div>
+    <Droppable droppableId={`${props.id}`}>
+      {(provided) => (
+        <div className={styles.column}>
+          <div className={styles.container}>
+            <h4 className={styles.title}>{props.title}</h4>
+            <img
+              className={styles.image}
+              src={deleteImg}
+              alt="delete"
+              onClick={() => setOpenModal(true)}
+            />
+          </div>
+          <div className={styles.tasks} ref={provided.innerRef} {...provided.droppableProps}>
+            {props.tasks.map((task, index) => (
+              <Task key={task.id} id={task.id} task={task.task} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+          <AddButton title="task" />
+          <ConfirmationModal id={'1'} open={openModal} setOpen={setOpenModal} title="Column" />
+        </div>
+      )}
+    </Droppable>
   );
 }
 
