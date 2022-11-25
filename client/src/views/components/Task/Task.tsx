b4cec84d24668a01994af2d57be3f7b6cf41a6d0
Maria Guchkova <mariaguchkova1@gmail.com>
2022-11-25 14:09:05 +0300

    feat: delete task

--- client/src/views/components/Task/Task.tsx
+++ client/src/views/components/Task/Task.tsx
@@ -1,12 +1,33 @@
 import styles from './Task.module.scss';
+import closeImg from '../../../assets/icons/close-button.svg';
 import dotsImg from '../../../assets/icons/dots.svg';
+import { useState } from 'react';
+import ConfirmationModal from '../ConfirmationModal/ConfirmationModal';
 
-function Task(props: { task: string; id: string; columnsId: string }) {
+function Task(props: { task: string; columnsId: string; id: string }) {
+  const { task, columnsId, id } = props;
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div className={styles.task}>
-      <p>{props.task}</p>
-      <img className={styles.image} src={dotsImg} alt="edit" />
-    </div>
+    <>
+      <div className={styles.task}>
+        <p>{task}</p>
+        <img className={styles.image} src={dotsImg} alt="edit" onClick={() => setOpenModal(true)} />
+        <img
+          className={styles.image}
+          src={closeImg}
+          alt="close"
+          onClick={() => setOpenModal(true)}
+        />
+      </div>
+      <ConfirmationModal
+        id={id}
+        open={openModal}
+        setOpen={setOpenModal}
+        title="Task"
+        columnsId={columnsId}
+      />
+    </>
   );
 }
 
